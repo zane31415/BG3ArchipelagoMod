be6130e5172f650d5f775b391455ae1b58028861
zane31415 <zane314@gmail.com>
2025-09-19 20:16:09 -0700

    Replace item log with modvar, make char xp go to group, add kill log

--- Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/Mods/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/ScriptExtender/Lua/Server/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5.lua
+++ Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/Mods/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/ScriptExtender/Lua/Server/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5.lua
@@ -16,23 +16,24 @@
 
 -- SubQuestUpdateUnlocked ?
 
+Ext.Osiris.RegisterListener("KilledBy", 4, "after", function(defender, attackOwner, attacker, storyActionID)
+    local unparsed_kills = Ext.IO.LoadFile("killed_log.json")
+    if (unparsed_kills) then
+        local parsed_kills = Ext.Json.Parse(unparsed_kills)
+        table.insert(parsed_kills, defender)
+        Ext.IO.SaveFile("killed_log.json", Ext.Json.Stringify(parsed_kills))
+    end
+end)
+
 Ext.Osiris.RegisterListener("CharacterCreationFinished", 0, "after", function()
     print("CharCreationDone")
     Ext.IO.SaveFile("ap_out.json", "[]")
     Ext.IO.SaveFile("ap_in.json", "[]")
-    Ext.IO.SaveFile("sent.json", "[]")
+    --Ext.IO.SaveFile("killed_log.json", "[]")
 end)
 
 Ext.Osiris.RegisterListener("QuestUpdateUnlocked", 3, "after", function(character, topLevelQuestID, stateID)
     print("QuestUpdateUnlocked " .. character .. " " .. topLevelQuestID .. " " .. tostring(stateID))
-    --local avatars = Ext.Entity.GetAllEntitiesWithComponent("UserAvatar")
-    --for i, avatar in ipairs(avatars) do
-        --print("Avatar: " .. tostring(avatar))
-        --if (avatar.UserAvatarComponent.Character == character) then
-        --    print("Found avatar for " .. character)
-        --    break
-        --end
-    --end
 
     local unparsed = Ext.IO.LoadFile("ap_out.json")
     local data = {}
@@ -52,7 +53,6 @@ Ext.Osiris.RegisterListener("QuestUpdateUnlocked", 3, "after", function(characte
         end
     end
     if (needsToAdd) then
-        print("Adding quest state to ap_out.json: " .. topLevelQuestID .. "-" .. stateID)
         table.insert(data, topLevelQuestID .. "-" .. stateID)
         Ext.IO.SaveFile("ap_out.json", Ext.Json.Stringify(data))
     end
@@ -61,15 +61,11 @@ end)
 Ext.Osiris.RegisterListener("CastedSpell", 5, "after", function(caster, spell, spellType, spellElement, storyActionID)
     if (spell == "Shout_AP_Sync") then
         local unparsed_in = Ext.IO.LoadFile("ap_in.json")
-        local unparsed_sent = Ext.IO.LoadFile("sent.json")
-        local parsed_sent = {}
-        if (unparsed_sent) then
-            parsed_sent = Ext.Json.Parse(unparsed_sent)
-            if (parsed_sent == nil) then
-                print("Failed to parse sent.json")
-            end
-        end
         if (unparsed_in) then
+            local APModVars = Ext.Vars.GetModVariables(ModuleUUID)
+            if not APModVars.Sent then
+                APModVars.Sent = {}
+            end
             local data_in = Ext.Json.Parse(unparsed_in)
             if (data_in == nil) then
                 print("Failed to parse JSON")
@@ -77,32 +73,33 @@ Ext.Osiris.RegisterListener("CastedSpell", 5, "after", function(caster, spell, s
             end
             for k, v in ipairs(data_in) do
                 local isAlreadySent = false
-                for _, sentItem in ipairs(parsed_sent) do
-                    if (v == sentItem) then
-                        isAlreadySent = true
-                        break
-                    end
+                if (APModVars.Sent[v] == true) then
+                    isAlreadySent = true
                 end
                 if (not isAlreadySent) then
                     if (string.sub(v, 1, 5) == "Gold-") then
-                        local amount = tonumber(v:sub(6))
+                        local amount = tonumber(string.sub(v, 6, 11)) --Gold-100000-
                         print("Granting gold: " .. tostring(amount))
                         if (amount) then
                             AddGold(GetHostCharacter(), amount)
                         end
+                        APModVars.Sent[v] = true
                     elseif (string.sub(v, 1, 7) == "LevelUp") then
                         print("Granting exp")
-                        Osi.AddExplorationExperience(GetHostCharacter(), 1000000)
+                        local charTable = Osi.DB_Players:Get(nil)
+                        for char in pairs(charTable) do
+                            Osi.AddExplorationExperience(charTable[char][1], 1000000)
+                        end
+                        APModVars.Sent[v] = true
                     else
                         -- Assume item
                         print("Granting item: " .. v)
                         TemplateAddTo(v, GetHostCharacter(), 1)
+                        APModVars.Sent[v] = true
                     end
-                    table.insert(parsed_sent, v)
                 end
             end
         end
-        Ext.IO.SaveFile("sent.json", Ext.Json.Stringify(parsed_sent))
     end
 end)
-print("Archipelago Client Script Loaded v2")
+print("Archipelago Client Script Loaded v4")
