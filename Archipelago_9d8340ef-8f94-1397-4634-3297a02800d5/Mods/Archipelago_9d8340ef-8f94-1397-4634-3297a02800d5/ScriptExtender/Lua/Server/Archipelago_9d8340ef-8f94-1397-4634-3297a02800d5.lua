d860763fb434a19bea406d2fd3008976bcb97407
Broney <zane314@gmail.com>
2026-02-22 17:05:08 -0800

    Fix monster traps, add deathlink

--- Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/Mods/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/ScriptExtender/Lua/Server/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5.lua
+++ Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/Mods/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5/ScriptExtender/Lua/Server/Archipelago_9d8340ef-8f94-1397-4634-3297a02800d5.lua
@@ -19,11 +19,15 @@ PersistentVars = {}
 syncOnAny = true
 logKills = true
 logQuests = true
+deathlink = true
+pendingReceiveDeathlink = false
 importantKillSet = {
     ["S_HAG_Hag_c457d064-83fb-4ec6-b74d-1f30dfafd12d"] = true, -- Auntie Ethel
     ["S_FOR_Bottomless_SpiderQueen_e6b2f3ba-2d02-4507-8680-6047322e1a4b"] = true, -- Spider Queen
     ["S_UND_PetrifiedDrow_Spectator_39ff8241-fadd-4fbe-ab89-fc5a8b7638a0"] = true, -- Spectator
     ["S_UND_Bulette_307934b5-6fb5-4fdc-a7ff-433a7ba175b3"] = true, -- Bulette
+    ["S_UND_Myconid_BroodingSovereign_82af0858-d739-4c9d-84c8-5e6760e22e46"] = true, -- Glut
+    ["Kill-S_UND_MyconidSovereign_ea0f222f-eaad-4d83-bbcd-cbae51ccf265"] = true, -- Spaw
     ["S_UND_TheDrowNere_06bf05c5-216b-4eaf-91f5-8f1dd3d57f30"] = true, -- Nere
     ["S_UND_KethericCity_AdamantineGolem_2a5997fc-5f2a-4a13-b309-bed16da3b255"] = true, -- Grym
     ["S_CRE_Templar_378ac93e-03a0-40b4-904c-f37989ac7a8c"] = true, --Ch'r'ai W'wargaz
@@ -37,6 +41,28 @@ importantKillSet = {
     ["S_SHA_Necromancer_53651a9f-7ea8-444f-ba2d-224390b72f7d"] = true, -- Balthazar
     ["S_MOO_Ketheric_e9918f3e-5b87-40a3-a9bd-61545151573f"] = true -- Myrkul
 }
+
+deathlinkTriggers = {["S_Player_Karlach_2c76687d-93a2-477b-8b18-8a14b549304c"] = true,
+    ["S_Player_Minsc_0de603c5-42e2-4811-9dad-f652de080eba"] = true,
+    ["S_GOB_DrowCommander_25721313-0c15-4935-8176-9f134385451b"] = true,
+    ["S_GLO_Halsin_7628bc0e-52b8-42a7-856a-13a6fd413323"] = true,
+    ["S_Player_Jaheira_91b6b200-7d00-4d62-8dc9-99e8339dfa1a"] = true,
+    ["S_Player_Gale_ad9af97d-75da-406a-ae13-7071c563f604"] = true,
+    ["S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"] = true,
+    ["S_Player_Laezel_58a69333-40bf-8358-1d17-fff240d7fb12"] = true,
+    ["S_Player_Wyll_c774d764-4a17-48dc-b470-32ace9ce447d"] = true,
+    ["S_Player_ShadowHeart_3ed74f06-3c60-42dc-83f6-f034cb47c679"] = true}
+
+deathlinkNames = {["S_Player_Karlach_2c76687d-93a2-477b-8b18-8a14b549304c"] = "Karlach",
+    ["S_Player_Minsc_0de603c5-42e2-4811-9dad-f652de080eba"] = "Minsc",
+    ["S_GOB_DrowCommander_25721313-0c15-4935-8176-9f134385451b"] = "Minthara",
+    ["S_GLO_Halsin_7628bc0e-52b8-42a7-856a-13a6fd413323"] = "Halsin",
+    ["S_Player_Jaheira_91b6b200-7d00-4d62-8dc9-99e8339dfa1a"] = "Jaheira",
+    ["S_Player_Gale_ad9af97d-75da-406a-ae13-7071c563f604"] = "Gale",
+    ["S_Player_Astarion_c7c13742-bacd-460a-8f65-f864fe41f255"] = "Astarion",
+    ["S_Player_Laezel_58a69333-40bf-8358-1d17-fff240d7fb12"] = "Laezel",
+    ["S_Player_Wyll_c774d764-4a17-48dc-b470-32ace9ce447d"] = "Wyll",
+    ["S_Player_ShadowHeart_3ed74f06-3c60-42dc-83f6-f034cb47c679"] = "Shadowheart"}
 
 importantQuestSet = {
     ["DEN_Conflict-HalsinLeft_KilledLeaders"] = true,
@@ -74,13 +100,20 @@ function OnSessionLoaded()
         if (data.questsanity == 0) then
             logQuests = false
         end
+        if (data.deathlink == 0) then
+            deathlink = false
+        end
     end
 end
 
 Ext.Events.SessionLoaded:Subscribe(OnSessionLoaded)
 
 Ext.Osiris.RegisterListener("Died", 1, "after", function(died)
-    print("Died: " .. died)
+    print("Died: " .. tostring(died))
+    if (deathlink and deathlinkTriggers[died] and not pendingReceiveDeathlink) then
+        Ext.IO.SaveFile("deathLinkSend.json", '["' .. deathlinkNames[died] .. '"]')
+        pendingReceiveDeathlink = false
+    end
 end)
 
 Ext.Osiris.RegisterListener("KilledBy", 4, "after", function(defender, attackOwner, attacker, storyActionID)
@@ -144,6 +177,8 @@ Ext.Osiris.RegisterListener("CharacterCreationFinished", 0, "after", function()
         print("Resetting AP files")
         Ext.IO.SaveFile("ap_out.json", "[]")
         Ext.IO.SaveFile("ap_in.json", "[]")
+        Ext.IO.SaveFile("deathLinkSend.json", "[]")
+        Ext.IO.SaveFile("deathLinkReceive.json", "[]")
         
         PersistentVars['APSent'] = {}
     else
@@ -181,6 +216,23 @@ end)
 Ext.Osiris.RegisterListener("CastedSpell", 5, "after", function(caster, spell, spellType, spellElement, storyActionID)
     targetChar = GetHostCharacter()
     if (spell == "Shout_AP_Sync" or syncOnAny) then
+        if (deathlink) then
+            local unparsed_deathlink = Ext.IO.LoadFile("deathLinkReceive.json")
+            if unparsed_deathlink and unparsed_deathlink ~= "" then
+                local death_in = Ext.Json.Parse(unparsed_deathlink)
+                if (death_in == nil) then
+                    print("Failed to parse JSON")
+                    return
+                end
+                for k, v in ipairs(death_in) do
+                    if(v == "DeathLink") then
+                        pendingReceiveDeathlink = true
+                        Osi.Die(targetChar)
+                        Ext.IO.SaveFile("deathLinkReceive.json", "[]")
+                    end
+                end
+            end
+        end
         local unparsed_in = Ext.IO.LoadFile("ap_in.json")
         if (unparsed_in) then
             local APSent = PersistentVars['APSent']
@@ -214,7 +266,7 @@ Ext.Osiris.RegisterListener("CastedSpell", 5, "after", function(caster, spell, s
                         if (string.sub(v, 6, 13) == "Monster-") then
                             local monstername = string.sub(v, 14, 49)
                             local mon = Osi.CreateAtObject(monstername,targetChar,0,0,"",1)
-                            Osi.SetFaction(mon, "AP_TRAP_FACTION")
+                            Osi.SetFaction(mon, "ACT0a_TUT_HelmDevil_0314cde4-8572-4d70-a117-dba88e20e70d")
                             Osi.SetHostileAndEnterCombat(Osi.GetFaction(mon), Osi.GetFaction(targetChar), mon, targetChar)
                         elseif (string.sub(v, 6, 13) == "Bleeding") then
                             ApplyStatus(targetChar, "BLEEDING", 10)
